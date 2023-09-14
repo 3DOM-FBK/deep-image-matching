4637d73dfcd15a267d99dfd8b59cc8e488657968
Luca Morelli <lcmorelli.eng@gmail.com>
2023-09-14 14:38:00 +0200

    added matchers
    Former-commit-id: 24d9b4ff20845ab7e4d795ba547eca67bf6e4138

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 Install hloc. See https://github.com/cvg/Hierarchical-Localization
 
 # Example usage
-'''
+```
 python ./main.py -i assets/imgs -o assets/outs -r netvlad
-'''
+```
--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 # Example
-# python ./main.py -i assets/imgs -o assets/outs -r netvlad
+# python ./main.py -i assets/imgs -o assets/outs -m retreival -r netvlad
 
 import os
 import argparse
 
 from pathlib import Path
-from lib.image_retrieval import ImageRetrieval
+
+from lib.match_imgs import MatchImgs
 
 def main():
     parser = argparse.ArgumentParser(description="Matching with hand-crafted and deep-learning based local features and image retrieval.")
     
     parser.add_argument('-i', '--images', type=str, help="Input image folder")
     parser.add_argument('-o', '--outs', type=str, help="Output folder")
-    parser.add_argument('-r', '--retrieval', '--option', choices=['netvlad', 'openibl', 'cosplace', 'dir'])
+    parser.add_argument('-m', '--matcher', choices=['bruteforce', 'sequential', 'retreival'])
+    parser.add_argument('-r', '--retrieval', choices=['netvlad', 'openibl', 'cosplace', 'dir'])
     #parser.add_argument('-v', '--verbose', action='store_true', help="Enable verbose mode")
     
     args = parser.parse_args()
@@ -20,8 +22,10 @@ def main():
     imgs_dir = Path(args.images)
     output_dir = Path(args.outs)
     retrieval_option = args.retrieval
+    matching_option = args.matcher
     
-    ImageRetrieval(imgs_dir, output_dir, retrieval_option)
+    MatchImgs(matching_option, imgs_dir, output_dir, retrieval_option)
+    
 
 if __name__ == "__main__":
     main()
