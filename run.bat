678596dcd176c4027aff55895c0c218fb543cf0b
Luca Morelli <lmorelli@fbk.eu>
2023-10-17 15:40:15 +0200

    added pydegensac

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
 REM python ./main.py -i assets/imgs -o assets/outs -m custom_pairs -p ./assets/pairs.txt
 REM python ./main.py -i assets/imgs -o assets/outs -m sequential -v 1
-REM python ./main.py -i assets/imgs -o assets/outs -m sequential -f lightglue -n 8000 -v 1
-python ./main.py -i assets/imgs -o assets/outs -m sequential -f detect_and_describe -n 8000 -v 1
+python ./main.py -i assets/imgs -o assets/outs -m sequential -f loftr -n 8000 -v 1
+REM python ./main.py -i assets/imgs -o assets/outs -m sequential -f detect_and_describe -n 8000 -v 1
