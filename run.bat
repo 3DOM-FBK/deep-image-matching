c999db2e6a494ccecfae94fdfeeb681dd3a968f6
Luca Morelli <lmorelli@fbk.eu>
2023-10-17 20:19:53 +0200

    minor changes
    Former-commit-id: 36b7fb33f9b1251ff5700220c2cc3cfe2b0bea96

--- run.bat
+++ run.bat
@@ -1,5 +1,4 @@
 @echo off
-REM python ./main.py -i assets/imgs -o assets/outs -m custom_pairs -p ./assets/pairs.txt
-REM python ./main.py -i assets/imgs -o assets/outs -m sequential -v 1
+REM python ./main.py -i assets/imgs -o assets/outs -m custom_pairs -f loftr -n 8000 -p ./assets/pairs.txt
 python ./main.py -i assets/imgs -o assets/outs -m sequential -f loftr -n 8000 -v 1
 REM python ./main.py -i assets/imgs -o assets/outs -m sequential -f detect_and_describe -n 8000 -v 1
