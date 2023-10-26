9e37ab055b2052d7bfa5d7610d21aecd5e27c9cc
Luca Morelli <lmorelli@fbk.eu>
2023-10-26 21:28:56 +0200

    minor changes
    Former-commit-id: fa9ff7ed02661420516b4f8b491b7db22493197f

--- run.bat
+++ run.bat
@@ -1,5 +1,6 @@
 @echo off
 REM python ./main.py -i assets/imgs -o assets/outs -m custom_pairs -f loftr -n 8000 -p ./assets/pairs.txt
-python ./main.py cli -i assets/imgs -o assets/outs -m sequential -v 1 -f SuperPoint -n 8000
 REM python ./main.py -i assets/imgs -o assets/outs -m sequential -f detect_and_describe -n 8000 -v 1
 REM python ./main.py -i C:\Users\lmorelli\Desktop\Luca\GitHub_3DOM\Ferox\cam0\data -o assets/outs -m custom_pairs -f superglue -n 8000 -p C:\Users\lmorelli\Desktop\Luca\GitHub_3DOM\Ferox\pairs3.txt
+REM python ./main.py cli -i assets/imgs -o assets/outs -m sequential -v 1 -f superglue -n 8000
+python ./main.py gui -i assets/imgs -o assets/outs -m sequential -v 1 -f superglue -n 8000
