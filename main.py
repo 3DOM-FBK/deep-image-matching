12408525cef29161e458dcfb5141cd628635d390
Francesco Ioli <francesco.ioli@polimi.it>
2023-10-30 16:33:17 +0100

    major rewriting of main.py code
    Former-commit-id: 291b3418ce5ef4d687984805f8e3c2928cfd7bb3

--- main.py
+++ main.py
@@ -2,157 +2,225 @@ import argparse
 import shutil
 from pathlib import Path
 
-from easydict import EasyDict as edict
-
 from config import custom_config
-from src.deep_image_matching.gui import gui
 from src.deep_image_matching.image_matching import ImageMatching
 from src.deep_image_matching.io.export_to_colmap import ExportToColmap
-from src.deep_image_matching.utils import setup_logger
+from src.deep_image_matching.utils import change_logger_level, setup_logger
 
-logger = setup_logger(console_log_level="info")
+# TODO: improve configuation manamgement
+# The default configuration for each method (extractor and matchers)  must be defined inside each class.
+# The user should be able to override the default configuration by passing a dictionary to the constructor.
+# The user should be able to chose a configuration from a predefined list of configurations (e.g, confs_zoo) to be sure that the configuration is valid.
 
-features_zoo = ["superpoint", "alike", "orb", "disk", "keynetaffnethardnet"]
+logger = setup_logger(log_level="info")
 
-available_matchers_zoo = [
+
+features_zoo = [
+    "superpoint",
+    "alike",
+    "aliked",
+    "orb",
+    "disk",
+    "keynetaffnethardnet",
+    "sift",
+]
+matchers_zoo = [
     "superglue",
     "lightglue",
     "loftr",
+    "adalam",
 ]
+retrieval_zoo = ["netvlad", "openibl", "cosplace", "dir"]
 
-confs_zoo = []
+
+confs_zoo = {
+    "superpoint+lightglue": {"extractor": "superpoint", "matcher": "lightglue"},
+    "disk+lightglue": {"extractor": "disk", "matcher": "lightglue"},
+    # "aliked+lightglue": {"extractor": "aliked", "matcher": "lightglue"},
+    # "sift+lightglue": {"extractor": "sift", "matcher": "lightglue"},
+    "superpoint+superglue": {"extractor": "superpoint", "matcher": "superglue"},
+    "keynetaffnethardnet+adalam": {
+        "extractor": "keynetaffnethardnet",
+        "matcher": "adalam",
+    },
+    "loftr": {"extractor": None, "matcher": "loftr"},
+    # "alike": {"extractor": "alike", "matcher": None},
+    # "orb": {"extractor": "orb", "matcher": None},
+}
 
 
-def parse_args():
+def parse_args(debug: bool = False):
     parser = argparse.ArgumentParser(
         description="Matching with hand-crafted and deep-learning based local features and image retrieval."
     )
 
     parser.add_argument(
-        "interface", type=str, help="Run command line interface", choices=["cli", "gui"]
+        "--gui", action="store_true", help="Run command line interface", default=False
     )
 
+    parser.add_argument("-i", "--images", type=str, help="Input image folder")
     parser.add_argument(
-        "-i", "--images", type=str, help="Input image folder", required=True
+        "-o", "--outs", type=str, help="Output folder", default="output"
     )
-    parser.add_argument("-o", "--outs", type=str, help="Output folder", required=True)
+    parser.add_argument(
+        "-c",
+        "--config",
+        type=str,
+        help="Extactor and matcher configuration",
+        choices=confs_zoo.keys(),
+        default="superpoint+lightglue",
+    )
     parser.add_argument(
         "-m",
         "--strategy",
         choices=["bruteforce", "sequential", "retrieval", "custom_pairs"],
-        required=True,
+        default="sequential",
     )
-    parser.add_argument("-p", "--pairs", type=str)
+    parser.add_argument("-p", "--pairs", type=str, default=None)
     parser.add_argument(
         "-v",
         "--overlap",
         type=int,
-        help="Image overlap if using sequential overlap strategy",
+        help="Image overlap, if using sequential overlap strategy",
+        default=1,
     )
     parser.add_argument(
-        "-r", "--retrieval", choices=["netvlad", "openibl", "cosplace", "dir"]
+        "-r",
+        "--retrieval",
+        choices=retrieval_zoo,
+        default=None,
     )
-    parser.add_argument(
-        "-f",
-        "--features",
-        choices=[
-            "superglue",
-            "lightglue",
-            "loftr",
-            "ALIKE",
-            "ORB",
-            "DISK",
-            "SuperPoint",
-            "KeyNetAffNetHardNet",
-        ],
-    )
-    parser.add_argument("-n", "--max_features", type=int, required=True)
+    # parser.add_argument(
+    #     "-f",
+    #     "--features",
+    #     choices=[
+    #         "superglue",
+    #         "lightglue",
+    #         "loftr",
+    #         "ALIKE",
+    #         "ORB",
+    #         "DISK",
+    #         "SuperPoint",
+    #         "KeyNetAffNetHardNet",
+    #     ],
+    # )
+    parser.add_argument("-n", "--max_features", type=int, default=2048)
+    parser.add_argument("--debug", action="store_true", default=False)
 
     args = parser.parse_args()
+
+    if debug:
+        change_logger_level("debug")
+        args.images = Path("data/easy_small")
+        args.outs = Path("res/easy_small")
+        args.config = "superpoint+lightglue"
+        args.strategy = "sequential"
+        args.features = "superpoint"
+        args.matching = "lightglue"
+        args.retrieval = "netvlad"
+        args.overlap = 2
+        args.pairs = None
+        args.max_features = 4000
+
+        return args
+
+    # if args.interface == "gui":
+    #     gui_out = gui()
+    #     args
+
+    #     retrieval_option = None
+    #     if feat in ["superglue", "lightglue", "loftr"]:
+    #         local_features = feat
+    #     else:
+    #         local_features = "detect_and_describe"
+    #         custom_config["general"]["detector_and_descriptor"] = feat
+
+    # Checks for input arguments
+    if args.images is None:
+        raise ValueError("--images option is required")
+    else:
+        args.images = Path(args.images)
+        if not args.images.exists() or not args.images.is_dir():
+            raise ValueError(f"Folder {args.images} does not exist")
+    if args.outs is None:
+        args.outs = Path("output") / f"{args.images.name}_{args.config}_{args.strategy}"
+    else:
+        args.outs = Path(args.outs)
+        args.outs.mkdir(parents=True, exist_ok=True)
+
+    if args.config is None:
+        raise ValueError("--config option is required")
+    else:
+        args.local_features = confs_zoo[args.config]["extractor"]
+        args.matching = confs_zoo[args.config]["matcher"]
+
+    if args.strategy is None:
+        raise ValueError("--strategy option is required")
+    if args.strategy not in ["bruteforce", "sequential", "retrieval", "custom_pairs"]:
+        raise ValueError(
+            f"Invalid strategy option: {args.strategy}. Valid options are: bruteforce, sequential, retrieval, custom_pairs"
+        )
+    if args.strategy == "retrieval":
+        if args.retrieval is None:
+            raise ValueError(
+                "--retrieval option is required when --strategy is set to retrieval"
+            )
+        elif args.retrieval not in retrieval_zoo:
+            raise ValueError(
+                f"Invalid retrieval option: {args.retrieval}. Valid options are: {retrieval_zoo}"
+            )
+    else:
+        args.retrieval = None
+
+    if args.strategy == "custom_pairs":
+        if args.pairs is None:
+            raise ValueError(
+                "--pairs option is required when --strategy is set to custom_pairs"
+            )
+        args.pairs = Path(args.pairs)
+        if not args.pairs.exists():
+            raise ValueError(f"File {args.pairs} does not exist")
+    else:
+        args.pairs = None
+
+    if args.strategy == "sequential":
+        if args.overlap is None:
+            raise ValueError(
+                "--overlap option is required when --strategy is set to sequential"
+            )
+    else:
+        args.overlap = None
+
+    if args.debug:
+        change_logger_level("debug")
 
     return args
 
 
 def main(debug: bool = False):
-    if debug:
-        args = edict(
-            {
-                "interface": "gui",
-                "images": "data/easy_small",
-                "outs": "res/easy_small",
-                "strategy": "sequential",
-                "features": "lightglue",
-                "retrieval": "netvlad",
-                "overlap": 2,
-                "max_features": 1000,
-            }
-        )
-    else:
-        args = parse_args()
+    # Parse arguments
+    args = parse_args(debug=debug)
+    imgs_dir = args.images
+    output_dir = args.outs
+    matching_strategy = args.strategy
+    retrieval_option = args.retrieval
+    pair_file = args.pairs
+    overlap = args.overlap
+    max_features = args.max_features
 
-    if args.interface == "cli":
-        if args.strategy == "retrieval" and args.retrieval is None:
-            raise ValueError(
-                "--retrieval option is required when --strategy is set to retrieval"
-            )
-        elif args.strategy == "retrieval":
-            retrieval_option = args.retrieval
-        else:
-            retrieval_option = None
+    # temporary! Must be replaced by a configuration
+    if args.local_features in features_zoo:
+        local_features = args.local_features
+    if args.matching in matchers_zoo:
+        matching_method = args.matching
+    # else:
+    #     local_features = "detect_and_describe"
+    #     custom_config["general"]["detector_and_descriptor"] = args.local_features
 
-        if args.strategy == "custom_pairs" and args.pairs is None:
-            raise ValueError(
-                "--pairs option is required when --strategy is set to custom_pairs"
-            )
-        elif args.strategy == "custom_pairs":
-            pair_file = Path(args.pairs)
-        else:
-            pair_file = None
-
-        if args.strategy == "sequential" and args.overlap is None:
-            raise ValueError(
-                "--overlap option is required when --strategy is set to sequential"
-            )
-        elif args.strategy == "sequential":
-            overlap = args.overlap
-        else:
-            overlap = None
-
-        imgs_dir = Path(args.images)
-        output_dir = Path(args.outs)
-        matching_strategy = args.strategy
-        max_features = args.max_features
-
-        if not imgs_dir.exists() or not imgs_dir.is_dir():
-            raise ValueError(f"Folder {imgs_dir} does not exist")
-
-        if args.features in ["superglue", "lightglue", "loftr"]:
-            local_features = args.features
-        else:
-            local_features = "detect_and_describe"
-            custom_config["general"]["detector_and_descriptor"] = args.features
-
-    elif args.interface == "gui":
-        (
-            matching_strategy,
-            imgs_dir,
-            output_dir,
-            pair_file,
-            overlap,
-            feat,
-            max_features,
-        ) = gui()
-        retrieval_option = None
-        if feat in ["superglue", "lightglue", "loftr"]:
-            local_features = feat
-        else:
-            local_features = "detect_and_describe"
-            custom_config["general"]["detector_and_descriptor"] = feat
-
+    # Remove output folder if exists
     if output_dir.exists() and output_dir.is_dir():
         shutil.rmtree(output_dir)
     output_dir.mkdir(parents=True, exist_ok=True)
-
     custom_config["general"]["output_dir"] = output_dir
 
     # Generate pairs and matching
@@ -161,6 +229,7 @@ def main(debug: bool = False):
         matching_strategy=matching_strategy,
         retrieval_option=retrieval_option,
         local_features=local_features,
+        matching_method=matching_method,
         custom_config=custom_config,
         max_feat_numb=max_features,
         pair_file=pair_file,
@@ -201,6 +270,6 @@ def main(debug: bool = False):
 
 
 if __name__ == "__main__":
-    main(debug=True)
+    main(debug=False)
 
     logger.info("Done")
