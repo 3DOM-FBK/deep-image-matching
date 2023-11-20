937b1245239bb4b1c56cb5bce27c334547243574
lcmrl <lcmorelli.eng@gmail.com>
2023-11-20 16:10:05 +0100

    Flag to rotate upright images

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
 python ./main.py  --config superpoint+lightglue --images assets/example_images --outs assets/output --strategy sequential --overlap 1
+python ./main.py  --config superpoint+lightglue --images assets/example_images --outs assets/output --strategy sequential --overlap 1 --upright
