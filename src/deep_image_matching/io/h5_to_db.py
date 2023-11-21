23c54f87d5bbe815c07c239bf80b0ae81c36e769
Francesco Ioli <francesco.ioli@polimi.it>
2023-11-21 16:24:10 +0100

    updated join_databases.py
    Former-commit-id: 79392352dbcc09169a8ebad19efe3df9102a5889

--- src/deep_image_matching/io/h5_to_db.py
+++ src/deep_image_matching/io/h5_to_db.py
@@ -122,7 +122,7 @@ def add_matches(db, h5_path, fname_to_id):
 
                 matches = group[key_2][()]
                 db.add_matches(id_1, id_2, matches)
-                #db.add_two_view_geometry(id_1, id_2, matches)
+                db.add_two_view_geometry(id_1, id_2, matches)
 
                 added.add(pair_id)
 
@@ -150,8 +150,6 @@ def export_to_colmap(
         match_path,
         fname_to_id,
     )
-    
-
 
     db.commit()
     return
