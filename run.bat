c4b0ca6ed4e234c944499557b03fda2c04584cbb
Luca Morelli <lmorelli@fbk.eu>
2023-12-08 22:17:15 +0100

    Added examples

--- run.bat
+++ run.bat
@@ -1,3 +1,11 @@
 @echo off
-REM python ./main.py  --config superpoint+lightglue --images assets/example_images --outs assets/output --strategy sequential --overlap 1
-python ./main.py  --config superpoint+lightglue --images assets/example_images --outs assets/output --strategy sequential --overlap 1
+REM python ./main.py  --config superpoint+lightglue --images assets/example_cyprus --outs assets/output --strategy sequential --overlap 1
+REM python ./main.py  --config superpoint+lightglue --images assets/example_cyprus --outs assets/output --strategy sequential --overlap 1
+REM python ./main.py  --config superpoint+lightglue --images assets/example_cyprus --outs assets/output --strategy bruteforce --force
+REM python ./main.py  --config superpoint+lightglue_fast --images assets/example_cyprus --outs assets/output --strategy bruteforce --force
+python ./main.py  --config superpoint+superglue --images assets/example_cyprus --outs assets/output --strategy bruteforce --force
+REM python ./main.py  --config disk+lightglue --images assets/example_cyprus --outs assets/output --strategy bruteforce --force
+REM python ./main.py  --config aliked+lightglue --images assets/example_cyprus --outs assets/output --strategy bruteforce --force
+REM python ./main.py  --config orb+kornia_matcher --images assets/example_cyprus --outs assets/output --strategy bruteforce --force
+REM python ./main.py  --config sift+kornia_matcher --images assets/example_cyprus --outs assets/output --strategy bruteforce --force
+REM python ./main.py  --config keynetaffnethardnet+kornia_matcher --images assets/example_cyprus --outs assets/output --strategy bruteforce --force
