676dd8eefdaac9d6a4330f22e110fd260badad03
lcmrl <lcmorelli.eng@gmail.com>
2023-12-18 18:36:57 +0100

    Started generalization and cleaning join_databases.py
    Former-commit-id: 1b75f5e992e3d775a55bae8699d74a6618c76c95

--- run.sh
+++ run.sh
@@ -1,5 +1,27 @@
 #python ./main.py --help
-python ./main.py  --config superpoint+lightglue --images assets/example_cyprus --outs assets/output --strategy bruteforce --force
-#python ./main.py  --config superpoint+lightglue --images assets/example_cyprus --outs assets/output --strategy sequential --overlap 1 --force
-#python ./main.py  --config superpoint+lightglue --images assets/example_cyprus --outs assets/output --strategy sequential --overlap 1 --upright --force
-#python ./main.py  --config superpoint+lightglue --images assets/example_cyprus --outs assets/output --strategy matching_lowres --force --upright
+
+#SEQUENTIAL
+#python ./main.py  --config superpoint+lightglue --images assets/example_cyprus --outs assets/output --strategy sequential --overlap 1
+#python ./main.py  --config superpoint+lightglue --images assets/example_cyprus --outs assets/output --strategy sequential --overlap 1
+
+### BRUTEFORCE
+
+# example_cyprus
+# python ./main.py  --config superpoint+lightglue               --images assets/example_cyprus --outs assets/output --strategy bruteforce --force
+# python ./main.py  --config superpoint+lightglue               --images assets/example_cyprus --outs assets/output --strategy bruteforce --force --upright
+# python ./main.py  --config superpoint+lightglue_fast          --images assets/example_cyprus --outs assets/output --strategy bruteforce --force
+# python ./main.py  --config superpoint+superglue               --images assets/example_cyprus --outs assets/output --strategy bruteforce --force
+# python ./main.py  --config disk+lightglue                     --images assets/example_cyprus --outs assets/output --strategy bruteforce --force
+# python ./main.py  --config aliked+lightglue                   --images assets/example_cyprus --outs assets/output --strategy bruteforce --force
+# python ./main.py  --config orb+kornia_matcher                 --images assets/example_cyprus --outs assets/output --strategy bruteforce --force
+# python ./main.py  --config sift+kornia_matcher                --images assets/example_cyprus --outs assets/output --strategy bruteforce --force
+# python ./main.py  --config keynetaffnethardnet+kornia_matcher --images assets/example_cyprus --outs assets/output --strategy bruteforce --force
+# python ./main.py  --config keynetaffnethardnet+kornia_matcher --images assets/example_cyprus --outs assets/output --strategy bruteforce --force
+# python ./main.py  --config dedode                             --images assets/example_cyprus --outs assets/output --strategy bruteforce --force
+python ./main.py  --config loftr                             --images assets/example_cyprus --outs assets/output --strategy bruteforce --force
+
+# example_sacre_coeur
+# python ./main.py  --config loftr    --images assets/example_sacre_coeur --outs assets/output --strategy bruteforce --force
+# python ./main.py  --config se2loftr --images assets/example_sacre_coeur --outs assets/output --strategy bruteforce --force
+# python ./main.py  --config roma     --images assets/example_sacre_coeur --outs assets/output --strategy bruteforce --force
+# python ./main.py  --config se2loftr --images assets/example_sacre_coeur --outs assets/output --strategy bruteforce --force
