f7d9c80e0f994f94d9cbe2047fb1c4d5676d24e0
lcmrl <lcmorelli.eng@gmail.com>
2023-12-19 13:19:14 +0100

    minor changes

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@
 ### BRUTEFORCE
 
 # example_cyprus
-# python ./main.py  --config superpoint+lightglue               --images assets/example_cyprus --outs assets/output --strategy bruteforce --force
+python ./main.py  --config superpoint+lightglue               --images assets/example_cyprus --outs assets/output --strategy bruteforce --force
 # python ./main.py  --config superpoint+lightglue               --images assets/example_cyprus --outs assets/output --strategy bruteforce --force --upright
 # python ./main.py  --config superpoint+lightglue_fast          --images assets/example_cyprus --outs assets/output --strategy bruteforce --force
 # python ./main.py  --config superpoint+superglue               --images assets/example_cyprus --outs assets/output --strategy bruteforce --force
@@ -18,7 +18,7 @@
 # python ./main.py  --config keynetaffnethardnet+kornia_matcher --images assets/example_cyprus --outs assets/output --strategy bruteforce --force
 # python ./main.py  --config keynetaffnethardnet+kornia_matcher --images assets/example_cyprus --outs assets/output --strategy bruteforce --force
 # python ./main.py  --config dedode                             --images assets/example_cyprus --outs assets/output --strategy bruteforce --force
-python ./main.py  --config loftr                             --images assets/example_cyprus --outs assets/output --strategy bruteforce --force
+# python ./main.py  --config loftr                             --images assets/example_cyprus --outs assets/output --strategy bruteforce --force
 
 # example_sacre_coeur
 # python ./main.py  --config loftr    --images assets/example_sacre_coeur --outs assets/output --strategy bruteforce --force
