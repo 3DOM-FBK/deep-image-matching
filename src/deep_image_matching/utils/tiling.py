8e827412b44578f70c6d00f05c915e5795786812
Francesco Ioli <francesco.ioli@polimi.it>
2023-12-22 10:37:58 +0100

    updated tiler

--- src/deep_image_matching/utils/tiling.py
+++ src/deep_image_matching/utils/tiling.py
@@ -2,7 +2,6 @@ from enum import Enum
 from typing import Dict, List, Tuple, Union
 
 import kornia as K
-import matplotlib.pyplot as plt
 import numpy as np
 import torch
 
@@ -42,6 +41,14 @@ class Tiler:
                 "tiling_mode must be a TilingMode enum or a string with the name of the enum"
             )
         self._tiling_mode = tiling_mode
+
+    def compute_tiles(self, input: Union[np.ndarray, torch.Tensor], **kwargs):
+        if self._tiling_mode == TilingMode.SIZE:
+            return self.compute_tiles_by_size(input=input, **kwargs)
+        elif self._tiling_mode == TilingMode.GRID:
+            return self.compute_tiles_by_grid(input=input, **kwargs)
+        else:
+            return self.compute_tiles_auto(input=input, **kwargs)
 
     def compute_tiles_by_size(
         self,
@@ -131,197 +138,51 @@ class Tiler:
 
         return patches, origins, padding
 
-
-class Tiler_old:
-    """
-    Class for dividing an image into tiles.
-    """
-
-    def __init__(
+    def compute_tiles_by_grid(
         self,
+        input: Union[np.ndarray, torch.Tensor],
         grid: List[int] = [1, 1],
         overlap: int = 0,
         origin: List[int] = [0, 0],
-        max_length: int = 2000,
-    ) -> None:
-        """
-        Initialize class.
+    ) -> Tuple[Dict[int, np.ndarray], Dict[int, Tuple[int, int]]]:
+        raise NotImplementedError(
+            "compute_tiles_by_grid is not fully implemented yet (need to add padding and testing.)"
+        )
 
-        Parameters:
-        - grid (List[int], default=[1, 1]): List containing the number of rows and number of columns in which to divide the image ([nrows, ncols]).
-        - overlap (int, default=0): Number of pixels of overlap between adjacent tiles.
-        - origin (List[int], default=[0, 0]): List of coordinates [x, y] of the pixel from which the tiling starts (top-left corner of the first tile).
-        - max_length (int, default=2000): The maximum length of each tile.
+        if not isinstance(grid, list) or len(grid) != 2:
+            raise TypeError("grid must be a list of two integers")
 
-        Returns:
-        None
-        """
-        self._origin = origin
-        self._overlap = overlap
-        self._nrow = grid[0]
-        self._ncol = grid[1]
-        self._limits = None
-        self._tiles = None
+        if not isinstance(input, np.ndarray):
+            raise TypeError(
+                "input must be a numpy array. Tile selection by grid is not implemented for torch tensors yet."
+            )
 
-    @property
-    def grid(self) -> List[int]:
-        """
-        Get the grid size.
+        H, W = input.shape[:2]
+        n_rows = grid[0]
+        n_cols = grid[1]
 
-        Returns:
-        List[int]: The number of rows and number of columns in the grid.
-        """
-        return [self._nrow, self._ncol]
+        DX = round(W / n_cols / 10) * 10
+        DY = round(H / n_rows / 10) * 10
 
-    @property
-    def origin(self) -> List[int]:
-        """
-        Get the origin of the tiling.
+        origins = {}
+        for col in range(n_cols):
+            for row in range(n_rows):
+                tile_idx = np.ravel_multi_index((row, col), (n_rows, n_cols), order="C")
+                xmin = col * DX - overlap
+                ymin = row * DY - overlap
+                origins[tile_idx] = (xmin, ymin)
 
-        Returns:
-        List[int]: The coordinates [x, y] of the pixel from which the tiling starts (top-left corner of the first tile).
-        """
-        return self._origin
+        patches = {}
+        for idx, origin in origins.items():
+            xmin, ymin = origin
+            xmax = xmin + DX + overlap - 1
+            ymax = ymin + DY + overlap - 1
+            patches[idx] = input[ymin:ymax, xmin:xmax]
 
-    @property
-    def overlap(self) -> int:
-        """
-        Get the overlap size.
+        return patches, origins
 
-        Returns:
-        int: The number of pixels of overlap between adjacent tiles.
-        """
-        return self._overlap
-
-    @property
-    def limits(self) -> Dict[int, tuple]:
-        """
-        Get the tile limits.
-
-        Returns:
-        dict: A dictionary containing the index of each tile and its bounding box coordinates.
-        """
-        return self._limits
-
-    def compute_grid_size(self, max_length: int) -> None:
-        """
-        Compute the best number of rows and columns for the grid based on the maximum length of each tile.
-
-        Parameters:
-        - max_length (int): The maximum length of each tile.
-
-        Returns:
-        None
-
-        NOTE: NOT WORKING. NEEDS TO BE TESTED.
-        """
-        self._nrow = int(np.ceil(self._h / (max_length - self._overlap)))
-        self._ncol = int(np.ceil(self._w / (max_length - self._overlap)))
-
-    def compute_limits_by_grid(
-        self, image: np.ndarray
-    ) -> Tuple[Dict[int, tuple], Tuple[int, int]]:
-        """
-        Compute the limits of each tile (i.e., xmin, ymin, xmax, ymax) given the number of rows and columns in the tile grid.
-
-        Parameters:
-        - image (np.ndarray): The input image.
-
-        Returns:
-        Tuple[Dict[int, tuple], Tuple[int, int]]: A tuple containing two elements. The first element is a dictionary containing the index of each tile and its bounding box coordinates. The second element is a tuple containing the x, y coordinates of the pixel from which the tiling starts (top-left corner of the first tile).
-
-        Note:
-        - The input image should have shape (H, W, C).
-        """
-        self._image = image
-        self._w = image.shape[1]
-        self._h = image.shape[0]
-
-        DX = round((self._w - self._origin[0]) / self._ncol / 10) * 10
-        DY = round((self._h - self._origin[1]) / self._nrow / 10) * 10
-
-        self._limits = {}
-        for col in range(self._ncol):
-            for row in range(self._nrow):
-                tile_idx = np.ravel_multi_index(
-                    (row, col), (self._nrow, self._ncol), order="C"
-                )
-                xmin = max(self._origin[0], col * DX - self._overlap)
-                ymin = max(self._origin[1], row * DY - self._overlap)
-                xmax = xmin + DX + self._overlap - 1
-                ymax = ymin + DY + self._overlap - 1
-                self._limits[tile_idx] = (xmin, ymin, xmax, ymax)
-
-        return self._limits, self._origin
-
-    def extract_patch(self, image: np.ndarray, limits: List[int]) -> np.ndarray:
-        """
-        Extract an image patch given the bounding box coordinates.
-
-        Parameters:
-        - image (np.ndarray): The input image.
-        - limits (List[int]): The bounding box coordinates as [xmin, ymin, xmax, ymax].
-
-        Returns:
-        np.ndarray: The extracted image patch.
-        """
-        patch = image[
-            limits[1] : limits[3],
-            limits[0] : limits[2],
-        ]
-        return patch
-
-    def read_all_tiles(self) -> None:
-        """
-        Read all tiles and store them in the class instance.
-
-        Returns:
-        None
-        """
-        self._tiles = {}
-        for idx, limit in self._limits.items():
-            self._tiles[idx] = self.extract_patch(self._image, limit)
-
-    def read_tile(self, idx) -> np.ndarray:
-        """
-        Extract and return a tile given its index.
-
-        Parameters:
-        - idx (int): The index of the tile.
-
-        Returns:
-        np.ndarray: The extracted tile.
-        """
-        if self._tiles is None:
-            self._tiles = {}
-        return self.extract_patch(self._image, self._limits[idx])
-
-    def remove_tiles(self, tile_idx=None) -> None:
-        """
-        Remove tiles from the class instance.
-
-        Parameters:
-        - tile_idx: The index of the tile to be removed. If None, remove all tiles.
-
-        Returns:
-        None
-        """
-        if tile_idx is None:
-            self._tiles = {}
-        else:
-            self._tiles[tile_idx] = []
-
-    def display_tiles(self) -> None:
-        """
-        Display all the stored tiles.
-
-        Returns:
-        None
-        """
-        for idx, tile in self._tiles.items():
-            plt.subplot(self.grid[0], self.grid[1], idx + 1)
-            plt.imshow(tile)
-        plt.show()
+    def compute_tiles_auto(self, input: Union[np.ndarray, torch.Tensor]):
+        raise NotImplementedError("compute_tiles_auto is not implemented yet")
 
 
 if __name__ == "__main__":
