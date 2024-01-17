80d765fc6c7d7f56f4f5f45239b2f747db53fbec
Luca Morelli <lcmorelli.eng@gmail.com>
2024-01-17 16:19:38 +0100

    script to show matches from COLMAP database
    Former-commit-id: 66b3f35169b34678e17ca10d528a06ca88ca9830

---
+++ show_matches.py
@@ -0,0 +1,199 @@
+import os
+import cv2
+import glob
+import argparse
+import numpy as np
+
+from copy import deepcopy
+from tqdm import tqdm
+from pathlib import Path
+from src.deep_image_matching.utils.database import (
+    COLMAPDatabase,
+    blob_to_array,
+    pair_id_to_image_ids,
+)
+
+class ShowPairMatches:
+    def __init__(
+            self, 
+            database_path : Path, 
+            imgs_dict : dict,
+            imgs_dir : Path,
+            out_dir : Path,
+            ):
+
+        self.max_out_img_size = 1500 # pixel
+        self.db_path = database_path
+        self.imgs_dict = imgs_dict
+        self.imgs_dir = imgs_dir
+        self.out_dir = out_dir
+        self.imgs = {}
+        self.keypoints = {}
+        self.matches = {}
+        self.two_views_matches = {}
+
+        if self.db_path.suffix == '.db':
+            self.db_type = 'colmap'
+        else:
+            print("Error. Not supported database extension. Quit")
+            quit()
+
+    def LoadDatabase(self):
+        print('Loading database..')
+        if self.db_type == 'colmap':
+            db = COLMAPDatabase.connect(self.db_path)
+            self.imgs = dict(
+                (image_id, name)
+                for image_id, name in db.execute("SELECT image_id, name FROM images")
+            )
+            self.keypoints = dict(
+                (image_id, blob_to_array(data, np.float32, (-1, 2)))
+                for image_id, data in db.execute("SELECT image_id, data FROM keypoints")
+            )
+            for pair_id, r, c, data in db.execute("SELECT pair_id, rows, cols, data FROM matches"):
+                if data is not None:
+                    pair_id = pair_id_to_image_ids(pair_id)
+                    self.matches[(int(pair_id[0]), int(pair_id[1]))] = blob_to_array(
+                        data, np.uint32, (-1, 2)
+                    )
+            for pair_id, r, c, data in db.execute(
+                "SELECT pair_id, rows, cols, data FROM two_view_geometries"
+            ):
+                if data is not None:
+                    pair_id = pair_id_to_image_ids(pair_id)
+                    self.two_views_matches[(int(pair_id[0]), int(pair_id[1]))] = blob_to_array(
+                        data, np.uint32, (-1, 2)
+                    )
+
+    def ShowMatches(self):
+        if self.db_type == 'colmap':
+            self.ShowColmapMatches()
+
+    def ShowColmapMatches(self):
+        print("Showing matches..")
+        if self.imgs_dict['type'] == 'ids':
+            id0 = int(self.imgs_dict['data'][0])
+            keypoints0 = self.keypoints[id0]       
+            print(f"Img {id0}: kpts shape = {keypoints0.shape}")
+
+            id1 = int(self.imgs_dict['data'][1])
+            keypoints1 = self.keypoints[id1]
+            print(f"Img {id1}: kpts shape = {keypoints1.shape}")  
+
+            print("raw matches shape", np.shape(self.matches[(id0, id1)]))
+            print("verified matches shape", np.shape(self.two_views_matches[(id0, id1)]))
+
+            img0_path = self.imgs_dir / self.imgs[id0]
+            img1_path = self.imgs_dir / self.imgs[id1]
+            print("img0_path", img0_path)
+            print("img1_path", img1_path)
+
+            self.GeneratePlot(img0_path, img1_path, keypoints0, keypoints1, self.two_views_matches[(id0, id1)])
+    
+    def GeneratePlot(
+        self,
+        img0_path: Path,
+        img1_path: Path,
+        kpts0: np.ndarray,
+        kpts1: np.ndarray,
+        matches: np.ndarray,
+    ):
+        # Load images
+        img0 = cv2.imread(str(img0_path))
+        img1 = cv2.imread(str(img1_path))
+
+        # Convert keypoints to integers
+        kpts0_int = np.round(kpts0).astype(int)
+        kpts1_int = np.round(kpts1).astype(int)
+
+        # Create a new image to draw matches
+        img_matches = np.zeros((max(img0.shape[0], img1.shape[0]), img0.shape[1] + img1.shape[1], 3), dtype=np.uint8)
+        img_matches[:img0.shape[0], :img0.shape[1]] = img0
+        img_matches[:img1.shape[0], img0.shape[1]:] = img1
+
+        # Show keypoints
+        for kpt in kpts0_int:
+            kpt = tuple(kpt)
+            cv2.circle(img_matches, kpt, 3, (0, 0, 255), -1)
+
+        for kpt in kpts1_int:
+            kpt = tuple(kpt + np.array([img0.shape[1], 0]))
+            cv2.circle(img_matches, kpt, 3, (0, 0, 255), -1)
+
+        # Draw lines and circles for matches
+        for match in matches:
+            pt1 = tuple(kpts0_int[match[0]])
+            pt2 = tuple(np.array(kpts1_int[match[1]]) + np.array([img0.shape[1], 0]))
+
+            # Draw a line connecting the keypoints
+            cv2.line(img_matches, pt1, pt2, (0, 255, 0), 2)
+
+            # Draw circles around keypoints
+            cv2.circle(img_matches, pt1, 5, (255, 0, 0), -1)
+            cv2.circle(img_matches, pt2, 5, (255, 0, 0), -1)
+        
+        img_matches_resized = self.resize_image(img_matches, self.max_out_img_size)
+
+        ## Show the image with matches
+        #cv2.imshow(f"Verified matches   {img0_path.name} - {img1_path.name}", img_matches_resized)
+        #cv2.waitKey(0)
+        #cv2.destroyAllWindows()
+
+        cv2.imwrite(str(self.out_dir / f'matches_{img0_path.name}_{img1_path.name}.png'), img_matches_resized)
+
+    def resize_image(self, img, max_side_length):
+        height, width = img.shape[:2]
+
+        if max(height, width) > max_side_length:
+            scale_factor = max_side_length / max(height, width)
+            img_resized = cv2.resize(img, (int(width * scale_factor), int(height * scale_factor)))
+            return img_resized
+        else:
+            return img
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Show matches from COLMAP database."
+    )
+
+    parser.add_argument("-d", "--database", type=str, help="Path to COLMAP database", required=True)
+    parser.add_argument("-f", "--imgsdir", type=str, help="Path to images directory", required=True)
+    parser.add_argument("-i", "--images", type=str, help="Images IDs or names. E.g.: 'img1.jpg img2.jpg' or '37 98'. Max two images.", required=True)
+    parser.add_argument("-t", "--type", type=str, choices=["names", "ids"], required=True)
+    parser.add_argument("-o", "--output", type=str, help="Path to output folder", required=True)
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    args = parse_args()
+    database_path = Path(args.database)
+    out_dir = Path(args.output)
+    imgs_dir = Path(args.imgsdir)
+    print(f"database path: {database_path}")
+    print(f"output dir: {out_dir}")
+
+    i1, i2 = args.images.split()
+    imgs = {
+        "type" : args.type,
+        "data" : (i1, i2),
+    }
+    print("images: ", imgs)
+
+    show_pair_matches = ShowPairMatches(
+        database_path = database_path,
+        imgs_dict = imgs,
+        imgs_dir = imgs_dir,
+        out_dir = out_dir,
+    )
+
+    show_pair_matches.LoadDatabase()
+    show_pair_matches.ShowMatches()
+
+
+
+
+
+if __name__ == "__main__":
+    main()
