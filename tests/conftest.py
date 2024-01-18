05bfc07cc6024a281a323139d0d09a38adebc613
Francesco Ioli <francesco.ioli@polimi.it>
2024-01-18 16:47:52 +0100

    added github actona for running tests and skateched first test

---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+import os
+from pathlib import Path
+
+import pytest
+
+
+@pytest.fixture
+def data_dir():
+    return Path(__file__).parents[0].parents[0] / "assets/example_cyprus"
+
+
+# @pytest.fixture
+# def log_dir():
+#     dirpath = tempfile.mkdtemp()
+#     return dirpath
+
+
+# @pytest.fixture
+# def cfg_file(data_dir):
+#     return data_dir / "config.yaml"
+
+
+# # @pytest.fixture
+# # def epoch_dict(data_dir):
+
+if __name__ == "__main__":
+    print(Path(os.path.split(__file__)[0]).parents[0] / "assets")
---
+++ tests/test_config.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+import pytest
+from deep_image_matching.config import Config
+
+
+@pytest.fixture
+def default_args():
+    return {
+        "gui": False,
+        "dir": Path(__file__).parents[0].parents[0] / "assets/example_cyprus",
+        "images": None,
+        "outs": None,
+        "config": "superpoint+lightglue",
+        "quality": "high",
+        "tiling": "none",
+        "strategy": "matching_lowres",
+        "pairs": None,
+        "overlap": None,
+        "retrieval": None,
+        "db_path": None,
+        "upright": False,
+        "skip_reconstruction": False,
+        "force": True,
+        "verbose": False,
+    }
+
+
+def test_config_initialization(default_args):
+    config = Config(default_args)
+    assert isinstance(config, Config)
+
+
+if __name__ == "__main__":
+    pytest.main()
