f7bc990dd906bc48a38fcb96a45ea2b74c3995eb
Francesco Ioli <francesco.ioli@polimi.it>
2024-01-18 19:12:52 +0100

    bump version 0.0.3 -> 0.0.4
    Former-commit-id: 7a3e55e314d03b0d39fd9e727ade105b7258ebe2

--- src/deep_image_matching/__init__.py
+++ src/deep_image_matching/__init__.py
@@ -4,7 +4,7 @@ from typing import Tuple
 from .utils.logger import change_logger_level, get_logger, setup_logger  # noqa: F401
 from .utils.timer import Timer, timeit  # noqa: F401
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 logger = setup_logger(name="deep-image-matching", log_level="info")
 timer = Timer(logger=logger)
