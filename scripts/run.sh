3db0826b1733f5dcc2a2d6bc99d07f07f5b6411c
Luca Morelli <lcmorelli.eng@gmail.com>
2024-01-24 15:04:59 +0100

    minor changes

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,38 +1,32 @@
-# Tests
-
-# bruteforce
-python ./main.py --config superpoint+lightglue --dir assets/pytest --strategy bruteforce --skip_reconstruction --force
-
-
 # SEQUENTIAL
-python ./main.py --config superpoint+lightglue --dir assets/pytest --strategy sequential --skip_reconstruction --overlap 1 --force
-python ./main.py --config superpoint+lightglue --dir assets/pytest --strategy sequential --overlap 1 --force
+#python ./main.py --pipeline superpoint+lightglue --dir assets/pytest --strategy sequential --skip_reconstruction --overlap 1 --force
+#python ./main.py --pipeline superpoint+lightglue --dir assets/pytest --strategy sequential --overlap 1 --force
 
-### BRUTEFORCE
+# BRUTEFORCE
+#python ./main.py --pipeline superpoint+lightglue --dir assets/pytest --strategy bruteforce --force --skip_reconstruction --config ./config/superpoint+lightglue.yaml
+#python ./main.py --pipeline superpoint+lightglue --dir assets/pytest --strategy bruteforce --force --upright
+#python ./main.py --pipeline superpoint+lightglue_fast --dir assets/pytest --strategy bruteforce --force --skip_reconstruction
+#python ./main.py --pipeline superpoint+superglue --dir assets/pytest --strategy bruteforce --force --skip_reconstruction --config ./config/superpoint+superglue.yaml
+#python ./main.py --pipeline disk+lightglue --dir assets/pytest --strategy bruteforce --force --skip_reconstruction
+#python ./main.py --pipeline aliked+lightglue --dir assets/pytest --strategy bruteforce --force --skip_reconstruction
+#python ./main.py --pipeline orb+kornia_matcher --dir assets/pytest --strategy bruteforce --force
+#python ./main.py --pipeline sift+kornia_matcher --dir assets/pytest --strategy bruteforce --force --skip_reconstruction --config ./config/sift.yaml
+#python ./main.py --pipeline keynetaffnethardnet+kornia_matcher --dir assets/pytest --strategy bruteforce --force
+##python ./main.py --pipeline dedode --dir assets/pytest --strategy bruteforce --force --skip_reconstruction
+#python ./main.py --pipeline roma --dir assets/pytest --strategy bruteforce --force --skip_reconstruction --quality high --tiling none -V --config ./config/roma.yaml
+#python ./main.py --pipeline roma --dir assets/example_temple_baal --strategy bruteforce --force --skip_reconstruction --quality high --tiling none -V
+#python ./main.py --pipeline loftr --dir assets/pytest --strategy bruteforce --force --skip_reconstruction
 
-python ./main.py --config superpoint+lightglue --dir assets/pytest --strategy bruteforce --force --skip_reconstruction
-python ./main.py --config superpoint+lightglue --dir assets/pytest --strategy bruteforce --force --skip_reconstruction --upright
-python ./main.py --config superpoint+lightglue_fast --dir assets/pytest --strategy bruteforce --force --skip_reconstruction
-python ./main.py --config superpoint+superglue --dir assets/pytest --strategy bruteforce --force --skip_reconstruction
-python ./main.py --config disk+lightglue --dir assets/pytest --strategy bruteforce --force --skip_reconstruction
-python ./main.py --config aliked+lightglue --dir assets/pytest --strategy bruteforce --force --skip_reconstruction
-python ./main.py --config orb+kornia_matcher --dir assets/pytest --strategy bruteforce --force --skip_reconstruction
-python ./main.py --config sift+kornia_matcher --dir assets/pytest --strategy bruteforce --force --skip_reconstruction
-python ./main.py --config keynetaffnethardnet+kornia_matcher --dir assets/pytest --strategy bruteforce --force --skip_reconstruction
-python ./main.py --config dedode --dir assets/pytest --strategy bruteforce --force --skip_reconstruction
-python ./main.py --config roma --dir assets/pytest --strategy bruteforce --force --skip_reconstruction
-python ./main.py --config loftr --dir assets/pytest --strategy bruteforce --force --skip_reconstruction
+## EXAMPLE SACRE COEUR
+#python ./main.py --pipeline loftr --dir assets/example_sacre_coeur --strategy bruteforce --force
+#python ./main.py --pipeline se2loftr --dir assets/example_sacre_coeur --strategy bruteforce --force
+#python ./main.py --pipeline roma --dir assets/example_sacre_coeur --strategy bruteforce --force
+#python ./main.py --pipeline se2loftr --dir assets/example_sacre_coeur --strategy bruteforce --force
 
-# example_sacre_coeur
-python ./main.py --config loftr --dir assets/example_sacre_coeur --strategy bruteforce --force
-python ./main.py --config se2loftr --dir assets/example_sacre_coeur --strategy bruteforce --force
-python ./main.py --config roma --dir assets/example_sacre_coeur --strategy bruteforce --force
-python ./main.py --config se2loftr --dir assets/example_sacre_coeur --strategy bruteforce --force
-
-# examples
-python ./main.py --config superpoint+lightglue --dir assets/example_square --strategy bruteforce --force
-python ./main.py --config loftr --dir assets/example_easy_highres --strategy bruteforce --force
-python ./main.py --config loftr --dir assets/example_temple_bal --strategy bruteforce --force
-python ./main.py --config roma --dir assets/example_temple_bal --strategy bruteforce --force
-python ./main.py --config roma --dir assets/example_temple_bal --strategy retrieval -r cosplace --force
-python ./main.py --config roma --dir assets/example_square --strategy retrieval -r openibl --force
+## OTHER EXAMPLES
+#python ./main.py --pipeline superpoint+lightglue --dir assets/example_square --strategy bruteforce --force
+#python ./main.py --pipeline loftr --dir assets/example_easy_highres --strategy bruteforce --force
+#python ./main.py --pipeline loftr --dir assets/example_temple_bal --strategy bruteforce --force
+#python ./main.py --pipeline roma --dir assets/example_temple_bal --strategy bruteforce --force
+#python ./main.py --pipeline roma --dir assets/example_temple_bal --strategy retrieval -r cosplace --force
+#python ./main.py --pipeline roma --dir assets/example_square --strategy retrieval -r openibl --force
