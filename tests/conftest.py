513e74b7d261048cacd3a7e08ed7aeca035085a4
Francesco Ioli <francesco.ioli@polimi.it>
2024-01-26 15:30:34 +0100

    added tests for config

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,12 +5,18 @@ from pathlib import Path
 import pytest
 
 
-# Create a temporary directory with the test images for each test
+# # Create a temporary directory with the test images for each test
 @pytest.fixture
-def data_dir():
+def data_dir(request):
     assets = (Path(__file__).parents[0].parents[0] / "assets/pytest/images").resolve()
-
     tempdir = tempfile.mkdtemp()
     shutil.copytree(assets, Path(tempdir) / "images")
+
+    def cleanup():
+        # Cleanup code: remove the temporary directory and its contents
+        shutil.rmtree(tempdir)
+
+    # Add finalizer to the fixture
+    request.addfinalizer(cleanup)
 
     return tempdir
--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -1,6 +1,5 @@
 import platform
 import subprocess
-import tempfile
 from pathlib import Path
 
 import pytest
@@ -13,10 +12,11 @@ def script():
     return (Path(__file__).parents[1] / "main.py").resolve()
 
 
-# # Create a temporary directory with the test images for each test
-# @pytest.fixture
-# def data_dir():
-#     return (Path(__file__).parents[0].parents[0] / "assets/pytest").resolve()
+@pytest.fixture
+def config_file_tiling(data_dir):
+    config = {"general": {"tile_size": (200, 200)}}
+    config_file = Path(data_dir) / "temp.yaml"
+    return create_config_file(config, config_file)
 
 
 def run_pipeline(cmd, verbose: bool = False) -> None:
@@ -35,20 +35,15 @@ def run_pipeline(cmd, verbose: bool = False) -> None:
     ), f"Script execution failed with error: {stderr.decode('utf-8')}"
 
 
-def create_config_file(config: dict, path: Path, temporary: bool = False) -> Path:
+def create_config_file(config: dict, path: Path) -> Path:
     def tuple_representer(dumper, data):
         return dumper.represent_sequence("tag:yaml.org,2002:seq", data, flow_style=True)
 
     yaml.add_representer(tuple, tuple_representer)
 
-    if temporary:
-        with tempfile.NamedTemporaryFile("w", suffix=".yaml", delete=False) as file:
-            yaml.dump(config, file)
-            return Path(file.name)
-    else:
-        with open(path, "w") as f:
-            yaml.dump(config, f)
-            return Path(path)
+    with open(path, "w") as f:
+        yaml.dump(config, f)
+        return Path(path)
 
 
 # Test matching strategies
@@ -79,7 +74,7 @@ def test_sp_lg_custom_config(data_dir, script):
         }
     }
     config_file = Path(__file__).parents[1] / "temp.yaml"
-    config_file = create_config_file(config, config_file, temporary=False)
+    config_file = create_config_file(config, config_file)
     run_pipeline(
         f"python {script} --dir {data_dir} --pipeline superpoint+lightglue --config_file {config_file} --strategy sequential --overlap 1 --skip_reconstruction --force"
     )
@@ -136,14 +131,6 @@ def test_sp_lg_quality_medium(data_dir, script):
 
 
 # Test tiling
-@pytest.fixture
-def config_file_tiling():
-    config = {"general": {"tile_size": (200, 200)}}
-    config_file = Path(__file__).parents[1] / "temp.yaml"
-    config_file = create_config_file(config, config_file, temporary=False)
-    return config_file
-
-
 def test_tiling_preselection(data_dir, script, config_file_tiling):
     run_pipeline(
         f"python {script} --dir {data_dir} --pipeline superpoint+lightglue --strategy bruteforce --tiling preselection --config {config_file_tiling} --skip_reconstruction --force",
