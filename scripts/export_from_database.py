c3bb9975fea010c86d443b156f82d91d3ae3d3cd
Luca Morelli <lcmorelli.eng@gmail.com>
2024-02-01 14:47:19 +0100

    added exporting raw matches

--- scripts/export_from_database.py
+++ scripts/export_from_database.py
@@ -29,13 +29,30 @@ def dbReturnMatches(database_path, min_num_matches):
         cursor = connection.cursor()
 
         images = {}
+        raw_matches = {}
         matches = {}
+
         cursor.execute("SELECT image_id, camera_id, name FROM images;")
         for row in cursor:
             image_id = row[0]
             image_name = row[2]
             images[image_id] = image_name
 
+        # Raw matches
+        cursor.execute(
+            "SELECT pair_id, data FROM matches WHERE rows>=?;",
+            (min_num_matches,),
+        )
+
+        for row in cursor:
+            pair_id = row[0]
+            _matches = np.fromstring(row[1], dtype=np.uint32).reshape(-1, 2)
+            image_id1, image_id2 = pair_id_to_image_ids(pair_id)
+            image_name1 = images[image_id1]
+            image_name2 = images[image_id2]
+            raw_matches["{} {}".format(image_name1, image_name2)] = _matches
+
+        # Geometric verified matches
         cursor.execute(
             "SELECT pair_id, data FROM two_view_geometries WHERE rows>=?;",
             (min_num_matches,),
@@ -52,7 +69,7 @@ def dbReturnMatches(database_path, min_num_matches):
         cursor.close()
         connection.close()
 
-        return images, matches
+        return images, raw_matches, matches
 
     else:
         print("Database does not exist")
@@ -89,7 +106,6 @@ if __name__ == "__main__":
         description="Export keypoints and matches from a COLMAP database."
     )
     parser.add_argument("database", help="Path to COLMAP database.")
-    parser.add_argument("output_folder", help="Path to output folder.")
     parser.add_argument(
         "format",
         choices=["colmap", "x1y1x2y2"],
@@ -98,16 +114,20 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     database_path = Path(args.database)
-    out_dir = Path(args.output_folder)
+    out_dir = database_path.parent / "matches"
+    out_dir.mkdir(parents=True, exist_ok=True)
 
     images, keypoints = dbReturnKeypoints(database_path)
-    images, matches = dbReturnMatches(database_path, min_num_matches=1)
+    images, raw_matches, matches = dbReturnMatches(database_path, min_num_matches=1)
 
     # COLMAP FORMAT
     if args.format == "colmap":
         out_kpts_dir = out_dir / "keypoints"
         out_kpts_dir.mkdir(parents=True, exist_ok=False)
+        raw_matches_file = out_dir / "raw_matches.txt"
         matches_file = out_dir / "matches.txt"
+
+        # Export keypoints
         for id in keypoints:
             kpts = keypoints[id]
             kpt_file = open(f"{out_kpts_dir}/{images[id]}.txt", "w")
@@ -117,6 +137,17 @@ if __name__ == "__main__":
                 kpt_file.write(f"{kpts[row][0]} {kpts[row][1]} 1.0 0.0\n")
             kpt_file.close()
 
+        # Export raw matches
+        out_matches_file = open(raw_matches_file, "w")
+        for pair in raw_matches:
+            out_matches_file.write(f"{pair}\n")
+            tie_points = raw_matches[pair]
+            for row in range(tie_points.shape[0]):
+                out_matches_file.write(f"{tie_points[row][0]} {tie_points[row][1]}\n")
+            out_matches_file.write("\n")
+        out_matches_file.close()
+
+        # Export verified matches
         out_matches_file = open(matches_file, "w")
         for pair in matches:
             out_matches_file.write(f"{pair}\n")
@@ -128,15 +159,17 @@ if __name__ == "__main__":
 
     # "X1 Y1 X2 Y2" format
     if args.format == "x1y1x2y2":
-        for pair in matches:
+        images = {value: key for key, value in images.items()}
+
+        # Export raw matches
+        for pair in raw_matches:
             im1, im2 = pair.split(" ", 1)
             im1_no_format, _ = im1.split(".", 2)
             im2_no_format, _ = im2.split(".", 2)
-            matches_file = out_dir / f"{im1_no_format}_{im2_no_format}.txt"
+            matches_file = out_dir / f"raw_{im1_no_format}_{im2_no_format}.txt"
             out_matches_file = open(matches_file, "w")
 
-            tie_points = matches[pair]
-            images = {value: key for key, value in images.items()}
+            tie_points = raw_matches[pair]
             kpts1 = keypoints[images[im1]]
             kpts2 = keypoints[images[im2]]
 
@@ -152,12 +185,26 @@ if __name__ == "__main__":
 
             out_matches_file.close()
 
-        # for id in keypoints:
-        #    kpts = keypoints[id]
-        #    kpt_file = open(f"{out_kpts_dir}/{images[id]}.txt", "w")
-        #    kpt_file.write(f"{kpts.shape[0]} 128\n")
-        #    for row in range(kpts.shape[0]):
-        #        # kpt_file.write(f"{kpts[row][0]} {kpts[row][1]} {kpts[row][2]} {kpts[row][3]}\n")
-        #        kpt_file.write(f"{kpts[row][0]} {kpts[row][1]} 1.0 0.0\n")
-        #    kpt_file.close()
-#
+        # Export verified matches
+        for pair in matches:
+            im1, im2 = pair.split(" ", 1)
+            im1_no_format, _ = im1.split(".", 2)
+            im2_no_format, _ = im2.split(".", 2)
+            matches_file = out_dir / f"{im1_no_format}_{im2_no_format}.txt"
+            out_matches_file = open(matches_file, "w")
+
+            tie_points = matches[pair]
+            kpts1 = keypoints[images[im1]]
+            kpts2 = keypoints[images[im2]]
+
+            kpts1_ = kpts1[tie_points[:, 0]]
+            kpts2_ = kpts2[tie_points[:, 1]]
+
+            for r in range(tie_points.shape[0]):
+                x1 = kpts1_[r, 0]
+                y1 = kpts1_[r, 1]
+                x2 = kpts2_[r, 0]
+                y2 = kpts2_[r, 1]
+                out_matches_file.write(f"{x1} {y1} {x2} {y2}\n")
+
+            out_matches_file.close()
