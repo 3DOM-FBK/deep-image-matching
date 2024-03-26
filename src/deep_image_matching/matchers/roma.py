9c12be16203db0299a7e0131f523a8eb84213a28
Francesco Ioli <francesco.ioli@polimi.it>
2024-03-26 18:34:33 +0100

    updated roma to last version

--- src/deep_image_matching/matchers/roma.py
+++ src/deep_image_matching/matchers/roma.py
@@ -7,7 +7,7 @@ import numpy as np
 import torch
 from PIL import Image
 
-from .. import TileSelection, Timer, logger
+from .. import GeometricVerification, TileSelection, Timer, logger
 from ..io.h5 import get_features
 from ..thirdparty.RoMa.roma import roma_outdoor
 from ..utils.geometric_verification import geometric_verification
@@ -32,19 +32,35 @@ class RomaMatcher(DetectorFreeMatcherBase):
 
     grayscale = False
     as_float = True
-    max_tile_size = 448
-    max_tile_pairs = 50
-    keep_tiles = False
+    coarse_res = 784  # (560, 840)  # (h,w) or only one value for square images
+    upsample_res = 1120  # (864, 1296)  # (h,w) or only one value for square images
+    max_tile_pairs = 150  # 50
+    max_matches_per_pair = 10000
+    min_matches_per_tile = 3
+    max_matches_per_tile = 5000
+    keep_tiles = True  # False
 
     def __init__(self, config={}) -> None:
         super().__init__(config)
 
-        self.matcher = roma_outdoor(device=self._device)
-
         logger.warning(
-            "RoMa uses a fixed tile size of 448x448 pixels. This can result in an enormous amount of tiles with high resolution images. If this is your case, try to downscale images using a lower 'Quality' value."
+            f"RoMa uses a fixed tile size of {self.coarse_res} pixels. This can result in an enormous amount of tiles with high resolution images. If this is your case, try to downscale images using a lower 'Quality' value."
         )
-        self._config["general"]["tile_size"] = (self.max_tile_size, self.max_tile_size)
+        if isinstance(self.coarse_res, tuple):
+            tile_size = (self.coarse_res[1], self.coarse_res[0])
+        elif isinstance(self.coarse_res, int):
+            tile_size = (self.coarse_res, self.coarse_res)
+        else:
+            raise ValueError(
+                "Invalid type for 'coarse_res'. It should be an integer or a tuple of two integers."
+            )
+        self._config["general"]["tile_size"] = tile_size
+
+        self.matcher = roma_outdoor(
+            device=self._device,
+            coarse_res=self.coarse_res,
+            upsample_res=self.upsample_res,
+        )
 
     def match(
         self,
@@ -104,7 +120,8 @@ class RomaMatcher(DetectorFreeMatcherBase):
 
         # Rescale threshold according the image original image size
         img_shape = cv2.imread(str(img0)).shape
-        scale_fct = np.floor(max(img_shape) / self.max_tile_size / 2)
+        tile_size = max(self._config["general"]["tile_size"])
+        scale_fct = np.floor(max(img_shape) / tile_size / 2)
         gv_threshold = self._config["general"]["gv_threshold"] * scale_fct
 
         # Apply geometric verification
@@ -156,20 +173,15 @@ class RomaMatcher(DetectorFreeMatcherBase):
         img1_name = img1_path.name
 
         # Run inference
-        with torch.inference_mode():
-            # /home/luca/Desktop/gitprojects/github_3dom/deep-image-matching/src/deep_image_matching/thirdparty/RoMa/roma/models/matcher.py
-            # in class RegressionMatcher(nn.Module) def __init__ hardcoded self.upsample_res = (int(864/4), int(864/4))
-            W_A, H_A = Image.open(img0_path).size
-            W_B, H_B = Image.open(img1_path).size
+        W_A, H_A = Image.open(img0_path).size
+        W_B, H_B = Image.open(img1_path).size
 
-            warp, certainty = self.matcher.match(
-                str(img0_path), str(img1_path), device=self._device, batched=False
-            )
-            matches, certainty = self.matcher.sample(warp, certainty)
-            kptsA, kptsB = self.matcher.to_pixel_coordinates(
-                matches, H_A, W_A, H_B, W_B
-            )
-            kptsA, kptsB = kptsA.cpu().numpy(), kptsB.cpu().numpy()
+        warp, certainty = self.matcher.match(
+            str(img0_path), str(img1_path), device=self._device
+        )
+        matches, certainty = self.matcher.sample(warp, certainty)
+        kptsA, kptsB = self.matcher.to_pixel_coordinates(matches, H_A, W_A, H_B, W_B)
+        kptsA, kptsB = kptsA.cpu().numpy(), kptsB.cpu().numpy()
 
         # Create a 1-to-1 matching array
         matches0 = np.arange(kptsA.shape[0])
@@ -241,14 +253,13 @@ class RomaMatcher(DetectorFreeMatcherBase):
             raise RuntimeError(
                 f"Too many tile pairs ({len(tile_pairs)}) to match, the matching process will be too slow and it may be inaccurate. Try to reduce the image resolution using a lower 'Quality' value (or change the 'max_tile_pairs' class attribute, if you know what you are doing)."
             )
-
+        else:
+            logger.info(f"Matching {len(tile_pairs)} tile pairs")
         timer.update("tile selection")
 
         # Read images and resize them if needed
         image0 = cv2.imread(str(img0))
         image1 = cv2.imread(str(img1))
-        image0.shape[:2]
-        image1.shape[:2]
         image0 = self._resize_image(self._quality, image0)
         image1 = self._resize_image(self._quality, image1)
 
@@ -265,6 +276,12 @@ class RomaMatcher(DetectorFreeMatcherBase):
         write_tiles_disk(tiles_dir / img1.name, tiles1)
         logger.debug(f"Tiles saved to {tiles_dir}")
 
+        # If max_matches_per_tile is not set, automatically compute it
+        if self.max_matches_per_tile < 0:
+            self.max_matches_per_tile = (
+                self.max_matches_per_pair // len(tile_pairs) * 10
+            )
+
         # Match each tile pair
         mkpts0_full = np.array([], dtype=np.float32).reshape(0, 2)
         mkpts1_full = np.array([], dtype=np.float32).reshape(0, 2)
@@ -280,32 +297,59 @@ class RomaMatcher(DetectorFreeMatcherBase):
             W_B, H_B = tiles1[tidx1].shape[1], tiles1[tidx1].shape[0]
 
             # Run inference
-            with torch.inference_mode():
-                warp, certainty = self.matcher.match(
-                    str(tile_path0), str(tile_path1), device=self._device, batched=False
+            warp, certainty = self.matcher.match(
+                str(tile_path0), str(tile_path1), device=self._device, batched=False
+            )
+            matches, certainty = self.matcher.sample(
+                warp, certainty, num=self.max_matches_per_tile
+            )
+            kptsA, kptsB = self.matcher.to_pixel_coordinates(
+                matches, H_A, W_A, H_B, W_B
+            )
+            kptsA, kptsB = kptsA.cpu().numpy(), kptsB.cpu().numpy()
+
+            # Get match confidence
+            conf = certainty.cpu().numpy()
+
+            # Do an intermediate and permessive geometric verification to reduce non-matched kpts in the final db
+            _, inlMask = geometric_verification(
+                kpts0=kptsA,
+                kpts1=kptsB,
+                method=GeometricVerification.PYDEGENSAC,
+                threshold=6,
+                confidence=0.99999,
+                quiet=True,
+            )
+            matches = matches[inlMask]
+            logger.debug(
+                f"  - Intermediate GV: {sum(inlMask)} ({sum(inlMask) / len(inlMask)*100:.2f}%) inliers in tile pair ({tidx0}, {tidx1})"
+            )
+            kptsA = kptsA[inlMask]
+            kptsB = kptsB[inlMask]
+
+            if self._config["general"]["verbose"]:
+                # For debugging
+                from ..visualization import viz_matches_cv2
+
+                tile_match_dir = (
+                    Path(self._config["general"]["output_dir"])
+                    / "debug"
+                    / "matches_by_tile"
                 )
-                matches, certainty = self.matcher.sample(warp, certainty)
-                kptsA, kptsB = self.matcher.to_pixel_coordinates(
-                    matches, H_A, W_A, H_B, W_B
+                tile_match_dir.mkdir(parents=True, exist_ok=True)
+                t0 = cv2.imread(str(tile_path0))
+                t1 = cv2.imread(str(tile_path1))
+                viz_matches_cv2(
+                    image0=t0,
+                    image1=t1,
+                    pts0=kptsA,
+                    pts1=kptsB,
+                    save_path=tile_match_dir
+                    / f"{img0.stem}-{img1.stem}_t{tidx0}-{tidx1}.jpg",
+                    line_thickness=1,
+                    autoresize=False,
+                    jpg_quality=60,
                 )
-                kptsA, kptsB = kptsA.cpu().numpy(), kptsB.cpu().numpy()
-
-                # Get match confidence
-                conf = certainty.cpu().numpy()
-
-            # For debugging
-            # from ..visualization import viz_matches_cv2
-
-            # viz_matches_cv2(
-            #     tiles0[tidx0],
-            #     tiles1[tidx1],
-            #     kptsA,
-            #     kptsB,
-            #     f"sandbox/tile_{tidx0}-{tidx1}.png",
-            #     line_thickness=-1,
-            #     autoresize=False,
-            #     jpg_quality=80,
-            # )
 
             # Restore original image coordinates (not cropped)
             kptsA = kptsA + np.array(t_origins0[tidx0]).astype("float32")
@@ -343,6 +387,21 @@ class RomaMatcher(DetectorFreeMatcherBase):
             )
             mkpts0_full = mkpts0_full[unique_idx]
             mkpts1_full = mkpts1_full[unique_idx]
+
+        # For debugging
+        if self._config["general"]["verbose"]:
+            image0 = cv2.imread(str(img0))
+            image1 = cv2.imread(str(img1))
+            viz_matches_cv2(
+                image0,
+                image1,
+                mkpts0_full,
+                mkpts1_full,
+                save_path=tile_match_dir / f"{img0.stem}-{img1.stem}.jpg",
+                line_thickness=-1,
+                autoresize=True,
+                jpg_quality=60,
+            )
 
         # Create a 1-to-1 matching array
         matches0 = np.arange(mkpts0_full.shape[0])
