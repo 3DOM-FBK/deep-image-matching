061b756f48d4a6639ab49de96871ecc86a37dffd
Francesco Ioli <francesco.ioli@polimi.it>
2024-05-14 12:13:40 +0200

    clean thirdparty folder from unused stuff

--- src/deep_image_matching/thirdparty/DeDoDe/DeDoDe/__init__.py
+++
@@ -1,8 +0,0 @@
-from .model_zoo import (
-    dedode_detector_B,
-    dedode_detector_L,
-    dedode_descriptor_B,
-    dedode_descriptor_G,
-)
-
-DEBUG_MODE = False
--- src/deep_image_matching/thirdparty/DeDoDe/DeDoDe/model_zoo/__init__.py
+++
@@ -1,6 +0,0 @@
-from .dedode_models import (
-    dedode_detector_B,
-    dedode_detector_L,
-    dedode_descriptor_B,
-    dedode_descriptor_G,
-)
