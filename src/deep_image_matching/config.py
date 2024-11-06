ef513e1e1cf62848ad449275cca06a05f4fb6bb4
lcmrl <lcmorelli.eng@gmail.com>
2024-11-06 18:21:41 +0100

    switched to MAGSAC to speed up processing time

--- src/deep_image_matching/config.py
+++ src/deep_image_matching/config.py
@@ -44,7 +44,7 @@ conf_general = {
     #   GeometricVerification.PYDEGENSAC (use pydegensac),
     #   GeometricVerification.MAGSAC (use opencv MAGSAC),
     #   Other methods: RANSAC, LMEDS, RHO, USAC_DEFAULT, USAC_PARALLEL, USAC_FM_8PTS, USAC_FAST, USAC_ACCURATE, USAC_PROSAC, USAC_MAGSAC
-    "geom_verification": GeometricVerification.PYDEGENSAC,
+    "geom_verification": GeometricVerification.MAGSAC,
     "gv_threshold": 1,
     "gv_confidence": 0.99999,
     # Minimum number of inliers matches and minumum inlier ratio per pair
