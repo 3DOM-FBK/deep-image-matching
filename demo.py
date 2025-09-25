f9586c61476b2d27cf8b960069cbb5d187f51515
lcmrl <lcmorelli.eng@gmail.com>
2025-09-25 15:43:34 +0200

    options are parsed via command line as default

--- demo.py
+++ demo.py
@@ -11,26 +11,26 @@ start_time = time.time()
 
 logger = dim.setup_logger("dim")
 
-# Define the configuration parameters
-args = {
-    "dir": "./assets/example_cyprus",
-    "pipeline": "superpoint+lightglue", # ["superpoint+lightglue", "superpoint+lightglue_fast", "superpoint+superglue", "superpoint+kornia_matcher", "disk+lightglue", "aliked+lightglue", "orb+kornia_matcher", "sift+kornia_matcher", "loftr", "se2loftr", "roma", "srif", "keynetaffnethardnet+kornia_matcher", "dedode+kornia_matcher"]
-    "strategy": "bruteforce", # ["matching_lowres", "bruteforce", "sequential", "retrieval", "custom_pairs", "covisibility"]
-    "quality": "medium", # ["lowest", "low", "medium", "high", "highest"]
-    "tiling": "preselection", # ["none", "preselection", "grid", "exhaustive", "preselection_affine_transform"]
-    "camera_options": "./assets/example_cyprus/cameras.yaml",
-    "openmvg": None,
-    "force": True,  # Remove existing features and matches
-    "skip_reconstruction": False,
-    "graph": True,
-    #"upright": "custom", # ["custom", "2clusters", "exif"] With "custom" option, rotations must be specified in ./config/rotations.txt
-    #"config_file": "./config_superpoint_lightglue.yaml", # Path to custom config file (YAML format) for matcher
-    "verbose": False,
-}
+# Parse the parameters from the command line with
+from deep_image_matching.parser import parse_cli
+args = parse_cli()
 
-# Alternatively, you can parse the parameters from the command line with
-# from deep_image_matching.parser import parse_cli
-# args = parse_cli()
+# Alternatively, define the configuration parameters
+#args = {
+#    "dir": "./assets/example_cyprus",
+#    "pipeline": "superpoint+lightglue", # ["superpoint+lightglue", "superpoint+lightglue_fast", "superpoint+superglue", "superpoint+kornia_matcher", "disk+lightglue", "aliked+lightglue", "orb+kornia_matcher", "sift+kornia_matcher", "loftr", "se2loftr", "roma", "srif", "keynetaffnethardnet+kornia_matcher", "dedode+kornia_matcher"]
+#    "strategy": "bruteforce", # ["matching_lowres", "bruteforce", "sequential", "retrieval", "custom_pairs", "covisibility"]
+#    "quality": "medium", # ["lowest", "low", "medium", "high", "highest"]
+#    "tiling": "preselection", # ["none", "preselection", "grid", "exhaustive", "preselection_affine_transform"]
+#    "camera_options": "./assets/example_cyprus/cameras.yaml",
+#    "openmvg": None,
+#    "force": True,  # Remove existing features and matches
+#    "skip_reconstruction": False,
+#    "graph": True,
+#    #"upright": "custom", # ["custom", "2clusters", "exif"] With "custom" option, rotations must be specified in ./config/rotations.txt
+#    #"config_file": "./config_superpoint_lightglue.yaml", # Path to custom config file (YAML format) for matcher
+#    "verbose": False,
+#}
 
 # Build the configuration from the parameters or command line arguments
 config = dim.Config(args)
