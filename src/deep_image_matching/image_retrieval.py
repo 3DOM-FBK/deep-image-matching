55798f2b1f71d3628479d8eceeba16aceef04727
thuyennc <nguyencongthuyen@gmail.com>
2025-11-13 00:21:32 +0700

    improve pairs_no_duplicates generate

--- src/deep_image_matching/image_retrieval.py
+++ src/deep_image_matching/image_retrieval.py
@@ -8,9 +8,7 @@ def ImageRetrieval(imgs_dir, outs_dir, retrieval_option, sfm_pairs):
     max_track_length = 10  # Increase this number to increase the number of pairs
     if outs_dir.exists():
         shutil.rmtree(outs_dir)
-        os.mkdir(outs_dir)
-    else:
-        os.mkdir(outs_dir)
+    os.mkdir(outs_dir)
 
     number_imgs = len(os.listdir(imgs_dir))
     retrieval_conf = extract_features.confs[retrieval_option]
@@ -28,32 +26,19 @@ def ImageRetrieval(imgs_dir, outs_dir, retrieval_option, sfm_pairs):
         quit()
 
     img_pairs = []
-    with open(outs_dir / "retrieval_pairs.txt") as pairs:
-        lines = pairs.readlines()
-        for line in lines:
+    with open(outs_dir / "retrieval_pairs.txt", "r") as f:
+        for line in f:
             im1, im2 = line.strip().split(" ", 1)
             img_pairs.append((im1, im2))
 
-    index_duplicate_pairs = []
-    for i in range(len(img_pairs) - 1):
-        pair1 = img_pairs[i]
-        im1 = pair1[0]
-        im2 = pair1[1]
-        for j in range(i + 1, len(img_pairs)):
-            pair2 = img_pairs[j]
-            im3 = pair2[0]
-            im4 = pair2[1]
-            if im3 == im1 and im4 == im2 or im3 == im2 and im4 == im1:
-                index_duplicate_pairs.append(j)
-                # print('discarded', im1, im2, im3, im4)
-            else:
-                pass
-
+    unique_pairs = set()
     pairs = []
     with open(outs_dir / "pairs_no_duplicates.txt", "w") as final_pairs:
-        for i in range(len(img_pairs) - 1):
-            if i not in index_duplicate_pairs:
-                final_pairs.write(f"{img_pairs[i][0]} {img_pairs[i][1]}\n")
-                pairs.append((imgs_dir / img_pairs[i][0], imgs_dir / img_pairs[i][1]))
+        for im1, im2 in img_pairs:
+            pair_key = tuple(sorted((im1, im2)))
+            if pair_key not in unique_pairs:
+                unique_pairs.add(pair_key)
+                final_pairs.write(f"{im1} {im2}\n")
+                pairs.append((imgs_dir / im1, imgs_dir / im2))
 
     return pairs
